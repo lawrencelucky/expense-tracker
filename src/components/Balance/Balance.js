16215a661a19e5ad2bdeba625b187cdbd00a8fb1
lawrencelucky <lawrencelucky01@gmail.com>
2022-03-23 15:55:23 +0100

    addin  react currency format

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,9 +1,17 @@
+import CurrencyFormat from 'react-currency-format';
+
 import './balance.css';
 
 const Balance = (props) => {
   return (
     <div className='balance-container'>
-      <p>${props.balance}</p>
+      <CurrencyFormat
+        value={props.balance}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+        renderText={(balance) => <p>{balance}</p>}
+      />
     </div>
   );
 };
